7017c8b217168b403d9d857bc8d843fc81783e20
agry88 <rgok307085@gmail.com>
2025-11-09 16:16:42 +0800

    feat: enforce unique constraint on Post_PostTag model for post-tag relationships

--- post/models.py
+++ post/models.py
@@ -53,3 +53,6 @@ class Post_PostTag(models.Model):
     tag = models.ForeignKey(PostTag, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [["post", "tag"]]
